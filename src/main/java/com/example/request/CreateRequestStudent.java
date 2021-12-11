40c81ee51b7f906836aeeba7df55d681fb5d7344
Sritaj <sritajp@gmail.com>
2021-12-11 17:34:25 +0530

    added validation for firstname

--- src/main/java/com/example/request/CreateRequestStudent.java
+++ src/main/java/com/example/request/CreateRequestStudent.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class CreateRequestStudent {
 
     @JsonProperty("first_name")
+    @NotBlank(message = "First Name is Required!")
     private String firstName;
 
     private String lastName;
