c4b946553a4e4f24defceba686acc4d7a7a3edac
Sritaj <sritajp@gmail.com>
2021-12-11 18:16:12 +0530

    Added deleteStudent API with query param

--- src/main/java/com/example/service/StudentService.java
+++ src/main/java/com/example/service/StudentService.java
@@ -3,6 +3,7 @@ package com.example.service;
 import com.example.entity.Student;
 import com.example.repository.StudentRepository;
 import com.example.request.CreateRequestStudent;
+import com.example.request.DeleteStudentRequest;
 import com.example.request.UpdateStudentRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,5 +32,10 @@ public class StudentService {
             student.setFirstName(updateStudentRequest.getFirstName());
         }
         return studentRepository.save(student);
+    }
+
+    public String deleteStudent(long id){
+        studentRepository.deleteById(id);
+        return "Student has been deleted successfully";
     }
 }
