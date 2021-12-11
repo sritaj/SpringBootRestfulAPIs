120942a6a46f7e50d0a8d3df932eb56f482f5a06
Sritaj <sritajp@gmail.com>
2021-12-11 15:55:53 +0530

    Added missing Annotation

--- src/main/java/com/example/service/StudentService.java
+++ src/main/java/com/example/service/StudentService.java
@@ -3,9 +3,11 @@ package com.example.service;
 import com.example.entity.Student;
 import com.example.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class StudentService {
 
     @Autowired
