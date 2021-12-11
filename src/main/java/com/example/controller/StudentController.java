cc9b860a5562055f2592d36b23825f0d18c28112
Sritaj <sritajp@gmail.com>
2021-12-11 18:00:47 +0530

    Added student Update API

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import com.example.entity.Student;
 import com.example.request.CreateRequestStudent;
+import com.example.request.UpdateStudentRequest;
 import com.example.response.StudentResponse;
 import com.example.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +31,12 @@ public class StudentController {
     @PostMapping("create")
     public StudentResponse createStudent(@Valid @RequestBody CreateRequestStudent createRequestStudent) {
         Student student =  studentService.createStudent(createRequestStudent);
+        return new StudentResponse(student);
+    }
+
+    @PutMapping("update")
+    public StudentResponse updateStudent(@Valid @RequestBody UpdateStudentRequest updateStudentRequest) {
+        Student student =  studentService.updateStudent(updateStudentRequest);
         return new StudentResponse(student);
     }
 
