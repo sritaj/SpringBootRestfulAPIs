f8178bf4826fa697002aff31c9403f84a26eccb5
Sritaj <sritajp@gmail.com>
2021-12-11 18:16:42 +0530

    Added deleteStudent method

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -40,4 +40,9 @@ public class StudentController {
         return new StudentResponse(student);
     }
 
+    @DeleteMapping("delete")
+    public String deleteStudent(@RequestParam long id){
+        return studentService.deleteStudent(id);
+    }
+
 }
