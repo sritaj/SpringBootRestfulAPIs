b2b429df005230ce551d096e4d2db0adbce97493
Sritaj <sritajp@gmail.com>
2021-12-11 12:03:01 +0530

    Initial Test

---
+++ src/test/java/com/example/springbootrestapisdemo/SpringBootRestApisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootrestapisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
