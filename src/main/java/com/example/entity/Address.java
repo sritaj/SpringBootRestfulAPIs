9606448ae9da1d2de8c51d8d128f5744f657be94
Sritaj <sritajp@gmail.com>
2021-12-18 10:31:27 +0530

    Added Address entity class and repository

---
+++ src/main/java/com/example/entity/Address.java
@@ -0,0 +1,27 @@
+package com.example.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "Address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+}
