2b622321f3c7d2d78adc7f99ad62711edc1a36c9
Sritaj <sritajp@gmail.com>
2021-12-18 22:03:06 +0530

    Added Spring SOP example for Before

---
+++ src/main/java/com/example/aop/AspectConfig.java
@@ -0,0 +1,20 @@
+package com.example.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Aspect
+public class AspectConfig {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Before(value = "execution(* com.example.controller.*.*(..))")
+    public void beforeAdvice(JoinPoint joinPoint){
+        logger.info("Inside - Before Advice");
+    }
+}
