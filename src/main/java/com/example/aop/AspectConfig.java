bc01f939ff71c21b16349b2e48c274fce31c695d
Sritaj <sritajp@gmail.com>
2021-12-18 23:18:31 +0530

    Added Spring SOP example for After, AfterAdvice and Around

--- src/main/java/com/example/aop/AspectConfig.java
+++ src/main/java/com/example/aop/AspectConfig.java
@@ -1,8 +1,8 @@
 package com.example.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -18,10 +18,32 @@ public class AspectConfig {
 //        logger.info("Inside - Before Advice");
 //    }
 
-    @Before(value = "execution(* com.example.controller.*.*(..)) and args(Object)")
-    public void beforeAdviceWithOneParameter(JoinPoint joinPoint, Object object){
-        logger.info("Request = : " + object);
-    }
-
+//    @Before(value = "execution(* com.example.controller.*.*(..)) and args(Object)")
+//    public void beforeAdviceWithOneParameter(JoinPoint joinPoint, Object object){
+//        logger.info("Request = : " + object);
+//    }
+//
+//    @After(value = "execution(* com.example.controller.*.*(..)) and args(Object)")
+//    public void afterAdvice(JoinPoint joinPoint, Object object){
+//        logger.info("Request = : " + object);
+//    }
+//
+//    @AfterReturning(value = "execution(* com.example.controller.*.*(..)) and args(Object)", returning = "returningObject")
+//    public void afterReturningAdvice(JoinPoint joinPoint, Object object, Object returningObject){
+//        logger.info("Response = : " + returningObject);
+//    }
+//
+//    @Around(value = "execution(* com.example.controller.*.*(..)) and args(Object)")
+//    public void aroundAdvice(ProceedingJoinPoint proceedingJoinPoint, Object object){
+//        logger.info("Request = : " + object);
+//
+//        Object returningObject = null;
+//        try{
+//            returningObject = proceedingJoinPoint.proceed();
+//        }catch (Throwable e){
+//            e.printStackTrace();
+//        }
+//        logger.info("Response = : " + returningObject);
+//    }
 
 }
