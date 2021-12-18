29038ca4f4216190e19d3c3789240720bd49380f
Sritaj <sritajp@gmail.com>
2021-12-19 01:21:48 +0530

    Added example for scheduler with cron job

--- src/main/java/com/example/springbootrestapisdemo/SpringBootRestApisDemoApplication.java
+++ src/main/java/com/example/springbootrestapisdemo/SpringBootRestApisDemoApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @ComponentScan("com.example.*")
 @EntityScan("com.example.entity")
 @EnableJpaRepositories("com.example.repository")
+@EnableScheduling
 public class SpringBootRestApisDemoApplication {
 
     public static void main(String[] args) {
