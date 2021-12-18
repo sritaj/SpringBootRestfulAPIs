b6f39de7360685f8e6d90e08814a649036d26783
Sritaj <sritajp@gmail.com>
2021-12-18 22:02:38 +0530

    Added Log4J examples

--- src/main/java/com/example/controller/AppController.java
+++ src/main/java/com/example/controller/AppController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/")
 public class AppController {
 
+    Logger logger = LoggerFactory.getLogger(AppController.class);
+
     @Value("${app.name:Default Demo App}")
     private String appName;
 
     @GetMapping("/")
     public String getAppName() {
         return appName;
+    }
+
+    @GetMapping("getSample")
+    public String getSample(){
+        logger.error("Inside error");
+        logger.warn("Inside error");
+        logger.info("Inside info");
+        logger.debug("Inside debug");
+        logger.trace("Inside trace");
+        String example = "Example";
+        return example;
     }
 }
